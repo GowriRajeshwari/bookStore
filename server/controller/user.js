4bfcc34cb5adbc9699ea451109b66751eba72102
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-07-31 09:49:24 +0530

    [GowriK] Refactor : Removed the unwanted console.log from controller

--- server/controller/user.js
+++ server/controller/user.js
@@ -18,12 +18,9 @@ exports.registerUser = (req, res) => {
     let data = { message: "Invalid Input" };
     response.data = data;
     res.status(422).send(response);
-    console.log("error in registration invalid input", errors);
   } else {
-    console.log(req.body);
     userService.register(req, (err, data) => {
       if (err) {
-        console.log(err);
         response.success = false;
         response.message = err;
         res.status(404).send({ data: response });
@@ -37,8 +34,6 @@ exports.registerUser = (req, res) => {
   }
 };
 exports.loginUser = (req, res) => {
-  console.log("controller");
-  console.log(req.body);
   req.checkBody("email", "Email is invalid").notEmpty().isEmail();
   req.checkBody("password", "Password is invalid").notEmpty();
   var response = {};
@@ -47,20 +42,15 @@ exports.loginUser = (req, res) => {
     response.success = false;
     response.message = "Invalid Input";
     res.status(500).send(response);
-    console.log("error in registration invalid input", errors);
   } else {
-    console.log(req.body);
     userService.loginUser(req, (err, data) => {
       if (err) {
-        console.log(err);
         response.success = false;
         response.message = err;
-        console.log(err);
         res.status(500).send({ data: response });
       } else {
         response.success = true;
         response.data = data;
-        console.log(response);
         response.message = "Login Successfully";
         res.status(200).send({ data: response });
       }
@@ -69,7 +59,6 @@ exports.loginUser = (req, res) => {
 };
 //forgot Password
 exports.forgotPassword = (req, res) => {
-  console.log(req.body);
   req.checkBody("email", "Email is invalid").notEmpty().isEmail();
   var response = {};
   const errors = req.validationErrors();
@@ -77,29 +66,20 @@ exports.forgotPassword = (req, res) => {
     response.success = false;
     response.message = "Invalid Input";
     res.status(500).send(response);
-    console.log("error in registration invalid input", errors);
   } else {
     userService.forgotPassword(req, (err, data) => {
       if (err) {
-        console.log(err);
         response.success = false;
         response.message = err;
         res.status(500).send({ data: response });
       } else {
-        console.log("data");
         let data_id = data._id;
-        console.log(data_id);
         let obj = jwt.GenerateToken(data_id);
         let url = `http://localhost:3000/resetpassword`;
-
-        console.log(`${obj.token}`);
-        // response.cookie('auth',obj.token);
-        console.log("email", req.body.email);
         mailler.sendMailer(url, req.body.email);
         response.token = obj.token;
         response.success = true;
         response.data = data;
-        console.log(response);
         response.message = "Send Mail Successfully";
         res.status(200).send({ data: response });
       }
@@ -108,33 +88,26 @@ exports.forgotPassword = (req, res) => {
 };
 //reset Password
 exports.resetPassword = (req, res) => {
-  //checking the password is valid or not
   req.checkBody("password", "Password is invalid").notEmpty();
   req.checkBody("confirmPassword", "ConfirmPassword is invalid").notEmpty();
   var response = {};
   const error = req.validationErrors();
-  //validating the password and confirmpassword is equal
   if (req.body.password != req.body.confirmPassword) {
-    console.log("Password and confirm Password is not equal");
     res.send("Password and confirm password is not correct");
   }
   if (error) {
-    console.log(err);
     response.success = false;
     response.message = err;
     res.status(500).send({ data: response });
   } else {
     userService.resetPassword(req, (err, data) => {
       if (err) {
-        console.log(err);
         response.success = false;
         response.message = err;
         res.status(500).send({ data: response });
       } else {
-        //sending the response to client
         response.success = true;
         response.data = data;
-        console.log(response);
         response.message = "Reset Password Successfully";
         res.status(200).send({ data: response });
       }
