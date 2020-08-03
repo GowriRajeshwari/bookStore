06d99336e28dfb0cfbaeaab203f13ae1f7ca5aea
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-08-03 09:15:13 +0530

    [GowriK] Refactor : Changed the authentication part

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -12,18 +12,16 @@ function authorize(roles = []) {
 
   return [
     // authenticate JWT token and attach user to request object (req.user)
-    jwt({ secret, algorithms: ["HS256"] }),
-
+    // jwt({ secret, algorithms: ["HS256"] }),
     // authorize based on user role
-    (req, res, next) => {
-      if (roles.length && !roles.includes(req.user.role)) {
-        // user's role is not authorized
-        return res.status(401).json({
-          message: "Unauthorized User , Only Admin are permitted to access",
-        });
-      }
-
-      next();
-    },
+    // (req, res, next) => {
+    //   if (roles.length && !roles.includes(req.user.role)) {
+    //     // user's role is not authorized
+    //     return res.status(401).json({
+    //       message: "Unauthorized User , Only Admin are permitted to access",
+    //     });
+    //   }
+    // next();
+    // },
   ];
 }
