13ed7a50f5de0771a34c1fbd7b0d8192345a32c5
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-08-03 10:57:43 +0530

    [GowriK] Refactor : calling the authorization function in route

--- server/route/cartRoute.js
+++ server/route/cartRoute.js
@@ -4,6 +4,6 @@ const usercontroller = require("../controller/cart.js");
 const Role = require("../middleware/role.js");
 const authorize = require("../middleware/jwt.js");
 
-router.post("/cart", authorize(Role.Admin), usercontroller.addCart);
+router.post("/cart", authorize.authorize(), usercontroller.addCart);
 router.get("/:_id", usercontroller.getAllCart);
 module.exports = router;
