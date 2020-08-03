c10c195378d20c4ddaa7a906cfffa72adcd5ccfd
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-08-03 11:13:50 +0530

    [GowriK] Refactor : Authorization is done for adding the book

--- server/route/adminRoute.js
+++ server/route/adminRoute.js
@@ -4,7 +4,7 @@ const usercontroller = require("../controller/admin.js");
 const Role = require("../middleware/role.js");
 const authorize = require("../middleware/jwt.js");
 
-router.post("/books", authorize(Role.Admin), usercontroller.addBook);
+router.post("/books", authorize.authorize, usercontroller.addBook);
 router.get("/books", usercontroller.getAllBook);
 router.put("/update/:_id", usercontroller.updateBook);
 router.delete("/delete/:_id", usercontroller.deleteBook);
